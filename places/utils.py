1321acecfc01894d213ff3d9351d4a98a781d162
Jomari Macaraig <macaraig.jomari10@gmail.com>
2018-09-27 03:13:53 +0000

    Added support for next page

--- places/utils.py
+++ places/utils.py
@@ -2,10 +2,11 @@ import googlemaps
 
 client = googlemaps.Client('AIzaSyCn3YGIibVn_wFzfhaFwsi759fEsvNuwUM')
 
-def find_place_ids(query):
-    places = client.places(query=query)
-    place_ids = [place['place_id'] for place in places['results']]
-    return place_ids
+def find_place_ids(query, page_token):
+    places = client.places(query=query, page_token=page_token)
+    results = places['results']
+    place_ids = [place['place_id'] for place in results]
+    return place_ids, results['next_page_token']
 
 def get_place_details(place_id):
     fields = ['name', 'formatted_address', 'website', 'formatted_phone_number']
