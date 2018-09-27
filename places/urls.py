b4305d697be0e8ac0d23a54419c9467cb44dbfde
unknown <applicant@HR-Test001.tlos.local>
2018-09-27 12:56:17 +0800

    Have working template, WIP

--- places/urls.py
+++ places/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from .views import PlaceView
+from .api import get_places
 
 urlpatterns = [
     path('', PlaceView.as_view(), name='place_view'),
+    path('api/places', get_places, name='get_places')
 ]
